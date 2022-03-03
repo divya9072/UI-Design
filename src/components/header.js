88155f8e4b3f6858c09804e46ec71e4d2a726b7b
divya9072 <divi9072@gmail.com>
2022-03-04 00:52:09 +0530

    changed with Style CSS

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,81 @@
 import React from "react";
-import "../styles/header.css";
+import styled from "styled-components";
 import Headerup from "./headerup";
 
+const Headers = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 12rem;
+  border-bottom: 2px solid #ddd;
+`;
+
+const Header_left = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 80px;
+  height: 80px;
+`;
+
+const Logo = styled.h1`
+  font-family: robutu;
+  color: rgb(36, 36, 36);
+`;
+const Header_right = styled.div``;
+
+const Lists = styled.ul`
+  display: flex;
+  list-style-type: none;
+`;
+
+const List = styled.li`
+  padding: 15px;
+  color: rgb(36, 36, 36);
+  font-weight: 700;
+  font-size: 15px;
+
+  :hover {
+    background-color: rgb(59, 194, 183);
+    cursor: pointer;
+  }
+`;
+
+const Search=styled.i`
+color: rgb(36, 36, 36);
+ cursor: "pointer";
+`;
 const Header = () => {
   return (
     <>
-    <Headerup/>
-    <div className="header">
-      <div className="header_left">
-        <img
-          style={{ width: "80px", height: "80px" }}
-          src="https://media.istockphoto.com/vectors/triangle-logo-upside-down-flipped-abstract-prism-geometric-shape-vector-id1279136583?b=1&k=20&m=1279136583&s=612x612&w=0&h=jmXIsQCcOe-23uv2_FH19AeNK6YHiS_mY7arNgPs_Bc="
-          alt="logo img"
-        />
-        <h1>STARTUPRR</h1>
-      </div>
-      <div className="header_right">
-        <ul>
-          <li>HOME</li>
-          <li>PORTFOLIO</li>
-          <li>BLOG</li>
-          <li>PAGES</li>
-          <li>FEATURES</li>
-          <li>MEGA MENU</li>
-          <li>CONTACT</li>
-          <li>
-            <i
-              style={{ color: " rgb(36, 36, 36)", cursor: "pointer" }}
-              class="fa fa-search"
-            ></i>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <Headerup />
+      <Headers>
+        <Header_left>
+          <Image
+            src="https://media.istockphoto.com/vectors/triangle-logo-upside-down-flipped-abstract-prism-geometric-shape-vector-id1279136583?b=1&k=20&m=1279136583&s=612x612&w=0&h=jmXIsQCcOe-23uv2_FH19AeNK6YHiS_mY7arNgPs_Bc="
+            alt="logo img"
+          />
+          <Logo>STARTUPRR</Logo>
+        </Header_left>
+        <Header_right>
+          <Lists>
+            <List>HOME</List>
+            <List>PORTFOLIO</List>
+            <List>BLOG</List>
+            <List>PAGES</List>
+            <List>FEATURES</List>
+            <List>MEGA MENU</List>
+            <List>CONTACT</List>
+            <List>
+              <Search>
+              <i class="fa fa-search"></i>
+              </Search>
+            </List>
+          </Lists>
+        </Header_right>
+      </Headers>
     </>
   );
 };
--- src/components/offercard.js
+++ src/components/offercard.js
@@ -1,17 +1,102 @@
 import React from "react";
-import "../styles/offer.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const OfferCard = ({ img,title, description }) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const Offercard_container = styled.div`
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1050px) {
+    flex-wrap: wrap;
+  }
+`;
+
+const Icon = styled.div`
+  font-size: 40px;
+  margin-bottom: 15px;
+`;
+
+const Offercard_box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  width: 400px;
+  height: auto;
+  box-sizing: border-box;
+  margin: 5px;
+  padding: 20px;
+
+  :hover {
+    box-shadow: 2px 2px 30px rgba(0, 0, 0, 0.2);
+    transition: all ease 0.3s;
+    cursor: pointer;
+
+    ${Icon} {
+      color: rgb(59, 194, 183);
+    }
+  }
+
+  @media (max-width: 1050px) {
+    flex-wrap: wrap;
+  }
+`;
+
+const Title = styled.h4`
+  letter-spacing: 0.5px;
+  margin-bottom: 8px;
+  color: rgb(102, 99, 99);
+  font-weight: 700;
+  line-height: 20px;
+
+  @media (max-width: 1050px) {
+    width: auto;
+    height: auto;
+  }
+`;
+const Desc = styled.span`
+  font-size: 14px;
+`;
+
+const Offercard_button = styled.button`
+  width: 110px;
+  height: 40px;
+  margin-top: 20px;
+  border-radius: 6px;
+  border: 1px solid #ddd;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: gray;
+  font-size: 700;
+  font-weight: 700;
+  background-color: white;
+
+  &:hover {
+    background-color: rgb(59, 194, 183);
+    color: white;
+  }
+`;
+
+const OfferCard = ({ img, title, description }) => {
   return (
     <div>
-      <div className="offercard_container">
-        <div className="offercard_box">
-          <FontAwesomeIcon icon={img} className='offercard_icons'></FontAwesomeIcon>
-          <h4>{title}</h4>
-          <span>{description}</span>
-          <button className="offercard_button">READ MORE</button>
-        </div>
-      </div>
+      <Offercard_container>
+        <Offercard_box>
+          <Icon>
+            <FontAwesomeIcon
+              icon={img}
+              // style={{ width: "15%", height: "100%", marginBottom: "1rem" }}
+            ></FontAwesomeIcon>
+          </Icon>
+          <Title>{title}</Title>
+          <Desc>{description}</Desc>
+          <Offercard_button>READ MORE</Offercard_button>
+        </Offercard_box>
+      </Offercard_container>
     </div>
   );
 };
